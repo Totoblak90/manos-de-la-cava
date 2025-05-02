0b2bd55f1d21d98c3ec7de29cbfb0687a055504a
Totoblak90 <tobias.blaksley@hotmail.com>
2025-05-02 15:49:38 -0300

    Improve angular.json config

---
+++ src/app/shared/header/header.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'mdlc-header',
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HeaderComponent {
+
+}
