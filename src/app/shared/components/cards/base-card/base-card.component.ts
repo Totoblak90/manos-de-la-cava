1d61acbbdbc94f2cdaf4c69de86adab5fd262b8c
Totoblak90 <tobias.blaksley@hotmail.com>
2025-05-06 23:12:35 -0300

    Fix BaseCardComponent

--- src/app/shared/components/cards/base-card/base-card.component.ts
+++ src/app/shared/components/cards/base-card/base-card.component.ts
@@ -1,10 +1,11 @@
 import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { Image } from '@mdlc/shared/models/image';
+import { ImageViewerComponent } from '../../image-viewer/image-viewer.component';
 
 @Component({
 	selector: 'mdlc-base-card',
-	imports: [RouterModule],
+	imports: [RouterModule, ImageViewerComponent],
 	templateUrl: './base-card.component.html',
 	styleUrl: './base-card.component.css',
 	changeDetection: ChangeDetectionStrategy.OnPush,
@@ -14,7 +15,7 @@ export class BaseCardComponent {
 	public readonly title = input.required<string>();
 
 	/** Description. */
-	public readonly description = input<string>();
+	public readonly description = input.required<string>();
 
 	/** Image URL. */
 	public readonly imageParams = input.required<Image>();
