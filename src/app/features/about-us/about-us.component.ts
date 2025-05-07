847f7df9e32ce74d366e17dfce5a310811009a9b
Totoblak90 <tobias.blaksley@hotmail.com>
2025-05-06 21:45:48 -0300

    Finish About Us Page

--- src/app/features/about-us/about-us.component.ts
+++ src/app/features/about-us/about-us.component.ts
@@ -1,12 +1,12 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ContentLayoutComponent } from '@mdlc/shared/components/content-layout/content-layout.component';
+import { ImageViewerComponent } from '@mdlc/shared/components/image-viewer/image-viewer.component';
 
 @Component({
 	selector: 'mdlc-about-us',
-	imports: [],
+	imports: [ContentLayoutComponent, ImageViewerComponent],
 	templateUrl: './about-us.component.html',
 	styleUrl: './about-us.component.css',
 	changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class AboutUsComponent {
-
-}
+export class AboutUsComponent {}
